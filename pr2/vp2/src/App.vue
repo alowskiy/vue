a2d86098bc2c86b31a1704a0cb7b76de7ebcdab3
N A <noobs2925@gmail.com>
2023-11-12 19:11:16 +0300

    init

---
+++ pr2/vp2/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default{
+
+
+data(){
+return{
+text: 'linkу',
+href: '/ajax'
+
+}
+
+}
+
+}
+
+</script>
+<template>
+  <a v-bind:href="href" @click.pevent="">{{ text }}</a>
+ 
+
+  </template>
